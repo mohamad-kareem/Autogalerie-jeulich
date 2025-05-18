2351ac20a40e0e908a97bd81f880eec70d4a6e92
mohamad-kareem <mohamadkareemeng@gmail.com>
2025-05-18 20:45:14 +0200

    fixing designs

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import NavBar from "./(components)/NavBar";
 import SessionWrapper from "@/app/(components)/SessionWrapper";
 import { Toaster } from "react-hot-toast";
+
 export const metadata = {
   title: "Autogalerie Jülich",
   description:
@@ -23,20 +24,12 @@ export const metadata = {
       },
     ],
   },
+  metadataBase: new URL("https://autogaleriejülich.de"),
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/mylogo.png" sizes="32x32" type="image/png" />
-        <link rel="shortcut icon" href="/mylogo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/mylogo.png" />
-        <link rel="canonical" href="https://autogaleriejülich.de/" />
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-
       <body>
         <Toaster
           position="top-center"
