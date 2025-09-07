c55c20c70dfba3accb53e6a54c2f418abd015d73
mohamad-kareem <mohamadkareemeng@gmail.com>
2025-09-07 22:55:41 +0200

    adding suspense

--- app/(Pages)/kaufvertrag/auswahl/page.jsx
+++ app/(Pages)/kaufvertrag/auswahl/page.jsx
@@ -1,6 +1,12 @@
+import { Suspense } from "react";
 import KaufvertragAuswahlPage from "./KaufvertragAuswahlPage";
 
 export default function Page({ searchParams }) {
-  const carId = searchParams.carId || "";
-  return <KaufvertragAuswahlPage carId={carId} />;
+  const carId = searchParams?.carId || "";
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <KaufvertragAuswahlPage carId={carId} />
+    </Suspense>
+  );
 }
